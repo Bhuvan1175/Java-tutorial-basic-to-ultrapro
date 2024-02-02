58f51daaa9bf56e7a90d3ad8815d750a34537f7f
Bhuvaneshwar Bagde <142175031+Bhuvan1175@users.noreply.github.com>
2024-02-03 00:19:34 +0530

    Add files via upload

---
+++ 04_Printing/flagWidth.java
@@ -0,0 +1,11 @@
+public class flagWidth {
+    public static void main(String[] args) {
+        int x=56;
+        float y=3.1f;
+        String str="java is";
+        System.out.printf("%(5d \n",x);
+        System.out.printf("%(5.1f \n",y);
+        System.out.printf("%20s",str);
+
+    }
+}
