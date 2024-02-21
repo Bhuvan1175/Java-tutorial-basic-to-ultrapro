e698315ddb8de3b2ff26f6576abbc709143d7024
Bhuvaneshwar Bagde <142175031+Bhuvan1175@users.noreply.github.com>
2024-02-21 17:49:24 +0530

    Add files via upload

---
+++ 11-Object Oriented/propertiesType.java
@@ -0,0 +1,10 @@
+// There are 3 type of property.
+// 1) Read and Writable
+// 2) Read Only
+// 3) Write Only
+// 1) Read and Writable : The property shows both getter and setter methods. It
+// means the value can be read as well write in this  property.
+// 2) Read Only : the property shows only getter property , it means we cannot set
+// any value to that particular property, but we can read that value.
+//  3) Write Only: The property shows only setter method . We can assign values to
+// that particular variable, but we cannot read that value.
--- 11-Object Oriented/rectangleClass.java
+++ 11-Object Oriented/rectangleClass.java
@@ -1,20 +1,40 @@
-class rectangle{
-    public int length;
-    public int breadth;
-    public int area(){
-        return length*breadth;
+class Rectangle {
+    private double length;
+    private double breadth;
+
+    public double getlength() {
+        return length;
     }
-    public int perimeter(){
-        return 2*(length+breadth);
+
+    public double getbreadth() {
+        return breadth;
+    }
+
+    public void setlength(double l) {
+        length = l;
+    }
+
+    public void setbreadth(double b) {
+        breadth = b;
+    }
+
+    public double Area() {
+        return length * breadth;
+    }
+
+    public double perimeter() {
+        return 2 * (length + breadth);
     }
 }
 
 public class rectangleClass {
     public static void main(String[] args) {
-        rectangle R1=new rectangle();
-        R1.length=5;
-        R1.breadth=7;
-        System.out.println("Area of Rectangle is: "+R1.area());
-        System.out.println("Perimeter of Rectangle is: "+R1.perimeter());
-    }    
+        Rectangle R1 = new Rectangle();
+        R1.setlength(10.5);
+        R1.setbreadth(6.7);
+        System.out.println("Area Of Rectangle :" + R1.Area());
+        System.out.println("Perimeter Of Rectangle :" + R1.perimeter());
+        System.out.println("Length :" + R1.getlength());
+        System.out.println("Breadth :" + R1.getbreadth());
+    }
 }
