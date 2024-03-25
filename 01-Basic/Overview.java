de0cba90b44a56c372f7f356a50a7873e8387110
Bhuvaneshwar Bagde <142175031+Bhuvan1175@users.noreply.github.com>
2024-03-25 20:22:54 +0530

    Update and rename Overview.txt to Overview.java

---
+++ 01-Basic/Overview.java
@@ -0,0 +1,22 @@
+Public : Access Modifier
+Static : Allows to call this method without creating an object of class.
+Void   : Return Type
+Main   : Function Name
+String Args[] : Command Line Arguments
+System : Class
+out    : Object of PrintStream
+println: Method in PrintStream
+Java Variable : JV Is the title of reserved memory location.
+Primative Data Type :
+1) Built in java
+2) Hold Single Value
+3) Cannot be divided
+Non Primative Datatype 
+1) User Defined
+2) Memory Address of a variable 
+3) complex 
+# MCQ 
+1) The compiler is not assign a default value to an uninitalized local variable in java programming.
+2) Double Data Type can store the longest decimal number.
+3) String cannot be stored in character data type.
+
