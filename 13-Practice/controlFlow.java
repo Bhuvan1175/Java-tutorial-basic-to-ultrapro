b94add39d914f738fd0a7baa14f89ee8f4aff178
Bhuvaneshwar Bagde <142175031+Bhuvan1175@users.noreply.github.com>
2024-08-16 05:22:20 +0530

    Create controlFlow.java

---
+++ 13-Practice/controlFlow.java
@@ -0,0 +1,11 @@
+public class controlFlow {
+    public static void main(String[] args) {
+        int a=7;
+        int b=8 , c=9;
+        if(a>b && b<c){
+            System.out.println("a is greater than b and b is smaller than c");
+        }else{
+            System.out.println("Error");
+        }
+    }
+}
